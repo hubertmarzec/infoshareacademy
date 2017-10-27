8066459ed561c45023d7d395fa04143362173321
Hubert Marzec <h.marzec@gmail.com>
2017-10-27 19:34:52 +0200

    simple react examples

---
+++ examples/react-example/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import List from './List';
+const App = function(props) {
+  return (
+    <div>
+      <h1>My todo list</h1>
+      <List/>
+    </div>
+  )
+};
+export default App;
